9f71676c7fb65b9f27fefabcd7bd6f422399a4b3
Dac Nguyen <huyng97@yahoo.com>
2022-05-01 19:11:53 -0500

    markdown previewer parser added

--- Front End Development Libraries Certification/7-front-end-projects/02-build-a-markdown-previewer/md.js
+++ Front End Development Libraries Certification/7-front-end-projects/02-build-a-markdown-previewer/md.js
@@ -1,1 +1,10 @@
 console.log('hi')
+console.log(marked.parse('separate file'));
+
+
+const preview = document.querySelector("#preview");
+const editorInput = document.querySelector("#editor");
+
+console.log(editorInput);
+
+preview.innerText = marked.parse('# hi there');
