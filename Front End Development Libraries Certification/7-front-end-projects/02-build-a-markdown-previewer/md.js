2fa049e82f0f9aa21f7be894c111026fe3a110b0
Dac Nguyen <huyng97@yahoo.com>
2022-05-02 21:43:58 -0500

    markdown previewer project completed

--- Front End Development Libraries Certification/7-front-end-projects/02-build-a-markdown-previewer/md.js
+++ Front End Development Libraries Certification/7-front-end-projects/02-build-a-markdown-previewer/md.js
@@ -1,10 +1,46 @@
-console.log('hi')
-console.log(marked.parse('separate file'));
-
-
 const preview = document.querySelector("#preview");
 const editorInput = document.querySelector("#editor");
 
-console.log(editorInput);
+const updatePreview = () => {
+  const editorInputValue = editorInput.value;
+  preview.innerHTML = marked.parse(editorInputValue);
+};
 
-preview.innerText = marked.parse('# hi there');
+editorInput.addEventListener("keyup", updatePreview);
+
+//onload actions:
+const defaultEditorValue = `\
+Welcome to my Editor, start editing below:
+
+# h1 example
+## h2 example
+[click to go to youtube](http://www.youtube.com)
+
+\` example of inline code\`
+
+\`\`\`
+function codeBlock() {
+  let codeExample = 'hello ice cream';
+  return codeExample;
+}
+\`\`\`
+
+- example
+- of 
+- a 
+- list
+
+> example of block quote
+
+### picture of a cute ice cream
+![ice cream pic](https://i.quotev.com/img/q/u/17/9/15/o3h2jozbyq.jpg)
+
+**bold example text**
+`;
+
+const defaultEditor = () => {
+  editorInput.value = defaultEditorValue;
+  preview.innerHTML = marked.parse(editorInput.value);
+};
+
+window.onload = defaultEditor;
