4dc2562a4d5c1f7e7129bf6b2fe6f08dea9053f4
Dac Nguyen <huyng97@yahoo.com>
2022-05-06 16:45:51 -0500

    drum machine completed added basic css

--- Front End Development Libraries Certification/7-front-end-projects/03-build-a-drum-machine/index.js
+++ Front End Development Libraries Certification/7-front-end-projects/03-build-a-drum-machine/index.js
@@ -1,50 +1,46 @@
 const displayNames = {
-  'Q': 'Bass Drum Sound',
-  'W': 'Snare Drum Hit',
-  'E': 'Rock Hi Hat',
-  'A': 'High Tom',
-  'S': '909 Mid Tom',
-  'D': '808 Mid Tom',
-  'Z': '909 Low Tom',
-  'X': 'Claves Bell',
-  'C': 'Hand Clap',
+ Q: "Bass Drum Sound",
+ W: "Snare Drum Hit",
+ E: "Rock Hi Hat",
+ A: "High Tom",
+ S: "909 Mid Tom",
+ D: "808 Mid Tom",
+ Z: "909 Low Tom",
+ X: "Claves Bell",
+ C: "Hand Clap",
 };
 
 const displayBeat = document.querySelector("#display");
 const drumContainer = document.querySelector("#drum-machine");
 
-drumContainer.addEventListener('click', e => {
-  if (e.target.tagName === 'BUTTON') {
-    let clickedBtnText = e.target.innerText;
-    let playClip = document.getElementById(clickedBtnText)
-    playClip.pause();
-    playClip.currentTime = 0;
-    playClip.play();
-  
-    displayBeat.innerText = displayNames[clickedBtnText];
-  
-    console.log(typeof e.target.innerText, 'click', clickedBtnText);
-  }
+drumContainer.addEventListener("click", (e) => {
+ if (e.target.tagName === "BUTTON") {
+  let clickedBtnText = e.target.innerText;
+  let playClip = document.getElementById(clickedBtnText);
+  playClip.pause();
+  playClip.currentTime = 0;
+  playClip.play();
+
+  displayBeat.innerText = displayNames[clickedBtnText];
+
+  // console.log(typeof e.target.innerText, "click", clickedBtnText);
+ }
 });
 
 const displayNamesKeys = Object.keys(displayNames);
 console.log(displayNamesKeys);
 
-document.addEventListener('keypress', e => {
-  // console.log(typeof e.key, e.key);
-  // console.log(typeof e.key.toUpperCase(), e.key.toUpperCase());
+document.addEventListener("keypress", (e) => {
+ let keyPressed = e.key.toUpperCase();
+ let keyDisplayExist = displayNamesKeys.indexOf(keyPressed);
 
-  let keyPressed = e.key.toUpperCase()
-  let keyDisplayExist = displayNamesKeys.indexOf(keyPressed);
+ if (keyDisplayExist !== -1) {
+  // console.log(keyPressed);
+  let playClip = document.getElementById(keyPressed);
+  playClip.pause();
+  playClip.currentTime = 0;
+  playClip.play();
 
-  if (keyDisplayExist !== -1) {
-    console.log(keyPressed)
-    let playClip = document.getElementById(keyPressed)
-    playClip.pause();
-    playClip.currentTime = 0;
-    playClip.play();
-
-    displayBeat.innerText = displayNames[keyPressed];
-  }
+  displayBeat.innerText = displayNames[keyPressed];
+ }
 });
-
