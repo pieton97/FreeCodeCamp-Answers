4fa24b071550a5772aa184c349be315c761bd6b1
Dac Nguyen <huyng97@yahoo.com>
2022-05-09 18:21:02 -0500

    JS calculator FCC, 12/16 tests pass, basic operations work, need final js specific details

--- Front End Development Libraries Certification/7-front-end-projects/04-build-a-javascript-calculator/index.js
+++ Front End Development Libraries Certification/7-front-end-projects/04-build-a-javascript-calculator/index.js
@@ -1,5 +1,5 @@
+const topDisplay = document.getElementById("top-display");
 const display = document.getElementById("display");
-const clear = document.getElementById("clear");
 const numbers = document.querySelector(".numbers");
 const operations = document.querySelector(".operations");
 const equals = document.getElementById("equals");
@@ -9,17 +9,103 @@ let numStore2 = 0;
 
 let secondState = false;
 let previousAns = 0;
+let lastOperation;
 
+//if state, then cur val == numstore-1/2
 numbers.addEventListener('click', e => {
-  numStore1 += e.target.innerHTML;
+ if (e.target.tagName === "BUTTON") {
+  //appending numbers to storage
+  if (secondState == false) {
+    if (numStore1 == 0) {
+      numStore1 = e.target.innerText;
+    } else {
+      numStore1 += e.target.innerText;
+    }
+  } else if (secondState == true) {
+    if (numStore2 == 0) {
+      numStore2 = e.target.innerText;
+    } else {
+      numStore2 += e.target.innerText;
+    }
+  }
 
-  // console.log(e.target.innerHTML);
-  console.log(numStore1, typeof numStore1);
-  display.innerHTML = numStore1;
+  //displaying numbers from storage
+  if (secondState == false) {
+    topDisplay.innerText = `${numStore1}`;
+    display.innerText = numStore1;
+  } else {
+    topDisplay.innerText = `${numStore1} ${lastOperation} ${numStore2}`;
+    display.innerText = numStore2;
+  }
+ }
+  console.log(numStore1, typeof numStore1, numStore2, typeof numStore2);
 });
 
-/*
-AC clicked: set everything = to 0
+operations.addEventListener('click', e => {
+ if (e.target.tagName === "BUTTON") {
+  if (secondState == false) {
+    //clicking operation for first time
+    secondState = true;
+
+    lastOperation = e.target.innerText;
+
+    topDisplay.innerText = `${numStore1} ${lastOperation}`;
+    display.innerText = 0;
+  } else if (secondState == true && display.innerText == 0) {
+    //clicking operation after the first time, but want to change to diff operation
+    lastOperation = e.target.innerText;
+    topDisplay.innerText = `${numStore1} ${lastOperation}`;
+  } else if (secondState == true) {
+    //clicking an operation after first time
+
+    topDisplay.innerText = `${numStore1} ${lastOperation} ${numStore2}`;
+    display.innerText = 0;
+  } 
+ }
+});
+
+
+equals.addEventListener('click', e => {
+  if (e.target.tagName === "BUTTON") {
+    console.log(lastOperation); 
+    previousAns = calculate(lastOperation, numStore1, numStore2);
+
+    topDisplay.innerText = `${numStore1} ${lastOperation} ${numStore2} = ${previousAns}`;
+    display.innerText = previousAns;
+    
+    numStore1 = 0;
+    numStore2 = 0;
+    secondState = false;
+  }  
+});
+
+
+//calculator below
+const calculate = (operator, num1, num2) => {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+  return doOperation[operator](num1, num2);
+};
+
+var doOperation = {
+  '+': function (x, y) { return x + y },
+  '-': function (x, y) { return x - y },
+  'x': function (x, y) { return x * y },
+  '/': function (x, y) { return x / y },
+};
+
+const clearCalc = () => {
+  numStore1 = 0;
+  numStore2 = 0;
+  secondState = false;
+  display.innerText = numStore1;
+};
+
+
+
+/* ---my thinking process when starting the project:
+
+AC clicked: set everything = to 0, but not the previous answer
 
 nums clicked - appends clicked number to 
   store1 (secondState:false)
@@ -47,6 +133,11 @@ equal sign clicked:
 
  set display = math(store1 & store2)
  set prev-display = previousAns
+
+OTHER:
+if display length > 2 && display[0] == 0, delete first 0
+
+if decimal(.) btn clicked: indexof(.) exist: do nothing
 
 */
 
