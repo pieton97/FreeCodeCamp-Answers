356b6f8cedb1ab0ee49199be69e8d397e55ddee2
Dac Nguyen <huyng97@yahoo.com>
2022-05-10 15:01:02 -0500

    JS Calculator 16/16 works, just needs clean up and styling

--- Front End Development Libraries Certification/7-front-end-projects/04-build-a-javascript-calculator/index.js
+++ Front End Development Libraries Certification/7-front-end-projects/04-build-a-javascript-calculator/index.js
@@ -4,26 +4,43 @@ const numbers = document.querySelector(".numbers");
 const operations = document.querySelector(".operations");
 const equals = document.getElementById("equals");
 
-let numStore1 = 0;
-let numStore2 = 0;
+let numStore1 = '0';
+let numStore2 = '0';
 
 let secondState = false;
-let previousAns = 0;
+let previousAns = '0';
 let lastOperation;
 
 //if state, then cur val == numstore-1/2
 numbers.addEventListener('click', e => {
  if (e.target.tagName === "BUTTON") {
+  //flags if decimal already exists. default: false(decimal does not exist)
+  let flagStore1 = false;
+  let flagStore2 = false;
+  if (e.target.id == 'decimal') {
+    if (secondState == false && typeof numStore1 == 'string' && numStore1.indexOf('.') >= 0) {
+      flagStore1 = true;
+    } 
+    else if (secondState == true && typeof numStore2 == 'string' && numStore2.indexOf('.') >= 0) {
+      flagStore2 = true;
+    }
+  }
+
   //appending numbers to storage
-  if (secondState == false) {
+  if (secondState == false && flagStore1 == false) {
     if (numStore1 == 0) {
       numStore1 = e.target.innerText;
     } else {
       numStore1 += e.target.innerText;
     }
-  } else if (secondState == true) {
-    if (numStore2 == 0) {
+  } else if (secondState == true && flagStore2 == false) {
+    if (numStore2 === 0) {
       numStore2 = e.target.innerText;
+    } else if (numStore2 === '-0') {
+      //this removes the first 0 from a negative number. eg: (-04 ===> -4)
+      let indexOfZero = numStore2.indexOf('0');
+      numStore2 = numStore2.slice(0, indexOfZero)
+      numStore2 += e.target.innerText;
     } else {
       numStore2 += e.target.innerText;
     }
@@ -31,7 +48,7 @@ numbers.addEventListener('click', e => {
 
   //displaying numbers from storage
   if (secondState == false) {
-    topDisplay.innerText = `${numStore1}`;
+    // topDisplay.innerText = `${numStore1}`;
     display.innerText = numStore1;
   } else {
     topDisplay.innerText = `${numStore1} ${lastOperation} ${numStore2}`;
@@ -44,8 +61,14 @@ numbers.addEventListener('click', e => {
 operations.addEventListener('click', e => {
  if (e.target.tagName === "BUTTON") {
   if (secondState == false) {
-    //clicking operation for first time
+    //clicking operation for first time, moves us onto the second number storage
     secondState = true;
+
+      //this if statement initates if an operator is pressed after an = sign
+      if (numStore1 === '0') {
+        // console.log('yellow');
+        numStore1 = previousAns;
+      }
 
     lastOperation = e.target.innerText;
 
@@ -53,12 +76,25 @@ operations.addEventListener('click', e => {
     display.innerText = 0;
   } else if (secondState == true && display.innerText == 0) {
     //clicking operation after the first time, but want to change to diff operation
-    lastOperation = e.target.innerText;
-    topDisplay.innerText = `${numStore1} ${lastOperation}`;
-  } else if (secondState == true) {
-    //clicking an operation after first time
+    //
+    if (lastOperation != '-' && e.target.innerText == '-') {
+      console.log('allleoww');
+      numStore2 = '-' + numStore2;
+    } else {
+      numStore2 = 0;
+      lastOperation = e.target.innerText;
+      topDisplay.innerText = `${numStore1} ${lastOperation}`;
+    }
 
-    topDisplay.innerText = `${numStore1} ${lastOperation} ${numStore2}`;
+
+  } else if (secondState == true) {
+    //clicking an operation after first time, continued calculation
+    numStore1 = calculate(lastOperation, numStore1, numStore2);
+    lastOperation = e.target.innerText;
+    numStore2 = 0;
+    
+    
+    topDisplay.innerText = `${numStore1} ${lastOperation}`;
     display.innerText = 0;
   } 
  }
@@ -67,14 +103,14 @@ operations.addEventListener('click', e => {
 
 equals.addEventListener('click', e => {
   if (e.target.tagName === "BUTTON") {
-    console.log(lastOperation); 
+    // console.log(lastOperation); 
     previousAns = calculate(lastOperation, numStore1, numStore2);
 
     topDisplay.innerText = `${numStore1} ${lastOperation} ${numStore2} = ${previousAns}`;
     display.innerText = previousAns;
     
-    numStore1 = 0;
-    numStore2 = 0;
+    numStore1 = '0';
+    numStore2 = '0';
     secondState = false;
   }  
 });
@@ -98,12 +134,13 @@ const clearCalc = () => {
   numStore1 = 0;
   numStore2 = 0;
   secondState = false;
-  display.innerText = numStore1;
+  topDisplay.innerText = previousAns;
+  display.innerText = 0;
 };
 
 
 
-/* ---my thinking process when starting the project:
+/* ---inital rough draft of structure
 
 AC clicked: set everything = to 0, but not the previous answer
 
